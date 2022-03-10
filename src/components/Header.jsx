68077c14c95fab86398a80ce2da60f2ef954a550
BrunaScauri <brunascauri@hotmail.com>
2022-03-09 21:03:00 -0300

    Requisito 4

---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import md5 from 'crypto-js/md5';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class Header extends React.Component {
+    getImageGravatar = () => {
+      const { email } = this.props;
+      return md5(email.toString());
+    }
+
+    render() {
+      const { user } = this.props;
+
+      return (
+
+        <header>
+          <img
+            data-testid="header-profile-picture"
+            src={ `https://www.gravatar.com/avatar/${this.getImageGravatar()}` }
+            alt="gravatar"
+          />
+          <p data-testid="header-player-name">{ user }</p>
+          <p data-testid="header-score">0</p>
+        </header>
+      );
+    }
+}
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+  user: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+  user: state.user.user,
+});
+export default connect(mapStateToProps)(Header);
