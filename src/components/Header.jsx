5a800156f957f0c55d4d5411f1c73273f96cfcff
BrunaScauri <brunascauri@hotmail.com>
2022-03-09 21:48:56 -0300

    Tentativa de push 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ class Header extends React.Component {
       const { user } = this.props;
 
       return (
-      // comentário para push AAAAA
+      // comentário para push parte 2
         <header>
           <img
             data-testid="header-profile-picture"
